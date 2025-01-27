89a1b73d1d33fbd910f5071bceb988b8ee7a8ef2
Necromancy501 <94140246+Necromancy501@users.noreply.github.com.>
2025-01-28 01:06:43 +0800

    Add #valid_move recursive behavior

--- chess.rb
+++ chess.rb
@@ -7,5 +7,9 @@ puts game
 charlie = Player.new game, 'white'
 carlos = Player.new game, 'black'
 charlie.move_piece 'e2', 'e4'
-carlos.move_piece 'b8', 'c6'
+carlos.move_piece 'd7', 'd5'
+charlie.move_piece 'f1', 'b5' 
+carlos.move_piece 'd8', 'd6'
+charlie.move_piece 'd1', 'g4' 
+carlos.move_piece 'd6', 'e5'
 puts game
--- lib/player.rb
+++ lib/player.rb
@@ -14,6 +14,8 @@ class Player
     return if piece == nil || piece.color != @color
     piece_copy = piece.dup
 
+    p valid_moves piece, piece_coordinates
+
     if valid_move? piece, end_coordinates
       grid[piece_coordinates[0]][piece_coordinates[1]] = nil
       grid[end_coordinates[0]][end_coordinates[1]] = piece_copy
@@ -36,7 +38,46 @@ class Player
     [row, col]
   end
 
-  def valid_move? piece, finish_coordinates
+  #pending: fix repetition of value appends
+
+  def valid_moves piece, start_coordinates, direction = nil, moves_arr = [], i=0, piece_coordinates = nil
+    piece_coordinates ||= start_coordinates
+    grid = @board.position
+    moves = piece.figure.moves
+    direction = moves[i]
+    if moves.kind_of? Array
+      unless direction == nil
+        y, x = start_coordinates[0], start_coordinates[1]
+        next_move = [y-direction[1], x+direction[0]]
+        next_square = (next_move.all? { |value| (0..7).include?(value) }) ? (grid[next_move[0]]&.[](next_move[1])) : nil
+        if next_square == nil
+          in_bounds = next_move.all? { |value| (0..7).include?(value) }
+          if in_bounds
+            #return if mate enabler
+            moves_arr.append next_move
+            valid_moves piece, next_move, direction, moves_arr, i, piece_coordinates
+          end
+          i+=1
+          direction = moves[i]
+          valid_moves piece, piece_coordinates, direction, moves_arr, i, piece_coordinates
+        else
+          i+=1
+          direction = moves[i]
+          unless next_square.color == @color
+            moves_arr.append next_move
+          end
+          valid_moves piece, piece_coordinates, direction, moves_arr, i, piece_coordinates
+        end
+      else
+        return moves_arr
+      end
+    else
+      #hash valid moves behavior pending
+    end
+    moves_arr
+  end
+
+  def valid_move? piece, end_coordinates
     moves = piece.figure.symbol
     if moves.kind_of?(Array)
       false
